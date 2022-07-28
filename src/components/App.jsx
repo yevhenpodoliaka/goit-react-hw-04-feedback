0e5073804761200de7ff3a113c6341982047ad1f
Yevhen Podoliaka <zanp85740@gmail.com>
2022-07-28 09:43:51 +0300

    add useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,44 @@
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notafication/Notafication';
 
 function App() {
-  const [good, setGood] = useState(JSON.parse(window.localStorage.getItem('good'))??0);
-  const [neutral, setNetural] = useState(JSON.parse(window.localStorage.getItem('neutral'))??0);
-  const [bad, setBad] = useState(JSON.parse(window.localStorage.getItem('bad')) ?? 0);
+  const [good, setGood] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('good')) ?? 0;
+  });
+  
+  const [neutral, setNetural] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('neutral')) ?? 0;
+  });
+
+  const [bad, setBad] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('bad')) ?? 0;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem('good', good);
+  }, [good]);
+
+  useEffect(() => {
+    window.localStorage.setItem('bad', bad);
+  }, [bad]);
+
+  useEffect(() => {
+    window.localStorage.setItem('neutral', neutral);
+  }, [neutral]);
 
   const onButtonClick = e => {
-    const key = e.currentTarget.name;
-    switch (key) {
+    switch (e.currentTarget.name) {
       case 'good':
         setGood(good + 1);
-        window.localStorage.setItem('good',good + 1)
         break;
       case 'neutral':
         setNetural(neutral + 1);
-          window.localStorage.setItem('neutral',neutral + 1)
         break;
       case 'bad':
         setBad(bad + 1);
-         window.localStorage.setItem('bad',bad + 1)
         break;
 
       default:
